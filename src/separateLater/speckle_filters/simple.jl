677166d5e3ba85cb5bab18bd3ee4e0ef6d6b5d3f
Mac <kaaso@space.dtu.dk>
2022-12-20 18:24:04 +0100

    Start of Speckle Filter

---
+++ src/separateLater/speckle_filters/simple.jl
@@ -0,0 +1,10 @@
+
+function mean_filter(size =[3,3])
+    
+
+
+function speckle_mean_filter(img::Matrix{T},filter_size::Vector{Integer} = [5,5]) where T<: Real
+    
+
+
+end
---
+++ src/separateLater/speckle_filters/speckle_filters.jl
@@ -0,0 +1,8 @@
+include("../Sentinel1/Sentinel1.jl")
+include("../../SARProcessing.jl")
+include("../VisualiseSAR/VisualiseSAR.jl")
+
+
+include("simple.jl")
+
+
