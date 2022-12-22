9ba84305dd95fcc42a6cacb0ed705d46877752d8
lupemba <simon.kok@live.dk>
2022-12-22 18:51:04 +0100

    Replace missing values in DEM with NaN Update naming to new conventions

--- src/GeoCoding/DEM.jl
+++ src/GeoCoding/DEM.jl
@@ -5,23 +5,23 @@ abstract type DEM end
 ############# DEM Interface begin ######################################
 
 """
-    getCoordinate(dem::T,index::Tuple{Integer,Integer}) where T <: DEM
+    get_coordinate(dem::T,index::Tuple{Integer,Integer}) where T <: DEM
 
     Get the coordinate for a certain index in the DEM. 
     (All the DEM's are stored as a matrix of heights)
 """
-function getCoordinate(dem::T,index::Tuple{Integer,Integer}) where T <: DEM
+function get_coordinate(dem::T,index::Tuple{Integer,Integer}) where T <: DEM
      ErrorException("Not implemented for the following DEM type, $T")
 end
 
 
 """
-    getIndex(dem::T,coordinate::Tuple{Real,Real}) where T <: DEM
+    get_index(dem::T,coordinate::Tuple{Real,Real}) where T <: DEM
 
     Get the index of the DEM corresponding to the coordinate. 
     (All the DEM's are stored as a matrix of heights)
 """
-function getIndex(dem::T,coordinate::Tuple{Real,Real}) where T <: DEM
+function get_index(dem::T,coordinate::Tuple{Real,Real}) where T <: DEM
     ErrorException("Not implemented for the following DEM type, $T")
 end
 
@@ -34,44 +34,48 @@ end
     A DEM implementation for Tandem-X DEM data.
 """
 struct TandemxDEM <: DEM
-    heights::Array{Union{Missing,Float32},2}
-    coordinateOffset::Tuple{Float64,Float64} ## lat lon
-    coordinateSpacing::Tuple{Float64,Float64} ## lat lon
+    heights::Array{Float32,2}
+    coordinate_offset::Tuple{Float64,Float64} ## lat lon
+    coordinate_spacing::Tuple{Float64,Float64} ## lat lon
 end
 
-
+# No data value from documentation https://geoservice.dlr.de/web/dataguide/tdm90/
+const TANDEX_DEM_NO_DATA_VALUE = -32767.0
 
 """
-    loadTandemxDEM(tiffPath::String) -> TandemxDEM
+    load_tandemx_dem(tiffPath::String) -> TandemxDEM
 
     Load a Tandem-X DEM tiff. The Tandem-X DEM's can be downloaded from https://download.geoservice.dlr.de/TDM90/.
     Note: Invalid values are replaced with missing 
 """
-function loadTandemxDEM(tiffPath::String)
+function load_tandemx_dem(tiffPath::String)
     dataset = ArchGDAL.readraster(tiffPath)
 
     transform = ArchGDAL.getgeotransform(dataset)
 
-    coordinateOffset = (transform[4],transform[1])
-    coordinateSpacing = (transform[6],transform[2])
+    coordinate_offset = (transform[4],transform[1])
+    coordinate_spacing = (transform[6],transform[2])
+   
+    heights = dataset[:,:,1]
+    heights = permutedims(heights, (2, 1));
 
-    dataset = dataset[:,:,1]
-    dataset = permutedims(dataset, (2, 1));
+    # Remove values close to or below the no data value
+    invalid = heights .< (TANDEX_DEM_NO_DATA_VALUE + 1000)
+    heights[invalid] .= NaN
 
-    heights = Array{Union{Missing,Real},2}(missing,size(dataset)...)
-    valid = dataset .> -3000.0
-    heights[valid] .= dataset[valid]
+    # check that all ridicules values are removed
+    @assert !any(heights .< -1000) 
 
-    return TandemxDEM(heights, coordinateOffset, coordinateSpacing)
+    return TandemxDEM(heights, coordinate_offset, coordinate_spacing)
 end
 
 
-function getCoordinate(dem::TandemxDEM,index::Tuple{Integer,Integer})
-    return dem.coordinateOffset .+ (index.-1) .* dem.coordinateSpacing
+function get_coordinate(dem::TandemxDEM,index::Tuple{Integer,Integer})
+    return dem.coordinate_offset .+ (index.-1) .* dem.coordinate_spacing
 end
 
 
-function getIndex(dem::TandemxDEM,coordinate::Tuple{Real,Real})
-    return (coordinate .- dem.coordinateOffset) ./ dem.coordinateSpacing .+ 1
+function get_index(dem::TandemxDEM,coordinate::Tuple{Real,Real})
+    return (coordinate .- dem.coordinate_offset) ./ dem.coordinate_spacing .+ 1
 end
 
--- test/GeoCoding/DEMTest.jl
+++ test/GeoCoding/DEMTest.jl
@@ -1,89 +1,89 @@
 
-
+const TEST_DEM = "testData/largeFiles/TDM1_DEM__30_N38W028_V01_C/DEM/TDM1_DEM__30_N38W028_DEM.tif"
 
 function tandemxDEM_test()
     ## Arrange
-    heights = Array{Union{Missing,Float32},2}(missing, 5,5)
+    heights = fill(NaN32, 5,5)
     heights[2:3,3:5] .= 2.3
-    coordinateOffset = (30.5,20.5) ## lat lon
-    coordinateSpacing = (-0.1,0.2) ## lat lon
+    coordinate_offset = (30.5,20.5) ## lat lon
+    coordinate_spacing = (-0.1,0.2) ## lat lon
 
     ## Act
-    testdem = SARProcessing.TandemxDEM(heights, coordinateOffset, coordinateSpacing)
+    test_dem = SARProcessing.TandemxDEM(heights, coordinate_offset, coordinate_spacing)
     
     ## Assert
-    testOk = size(testdem.heights) == (5,5)
+    test_ok = size(test_dem.heights) == (5,5)
 
      
      ## Debug
-     if !testOk
+     if !test_ok
          println("Debug info: ", string(StackTraces.stacktrace()[1].func))
-         println("testdem: ", testdem)
+         println("test_dem: ", test_dem)
      end
  
-     return testOk
+     return test_ok
 
 end
 
 function coordinate_index_test()
     ## Arrange
-    heights = Array{Union{Missing,Float32},2}(missing, 5,5)
+    heights = fill(NaN32, 5,5)
     heights[2:3,3:5] .= 2.3
-    coordinateOffset = (30.5,20.5) ## lat lon
-    coordinateSpacing = (-0.1,0.2) ## lat lon
-    testdem = SARProcessing.TandemxDEM(heights, coordinateOffset, coordinateSpacing)
+    coordinate_offset = (30.5,20.5) ## lat lon
+    coordinate_spacing = (-0.1,0.2) ## lat lon
+    test_dem = SARProcessing.TandemxDEM(heights, coordinate_offset, coordinate_spacing)
 
     ## Act
-    coordinate_1 = SARProcessing.getCoordinate(testdem,(1,1))
-    coordinate_101 = SARProcessing.getCoordinate(testdem,(101,101))
-    index_101 = round.(Int,SARProcessing.getIndex(testdem,coordinate_101))
+    coordinate_1 = SARProcessing.get_coordinate(test_dem,(1,1))
+    coordinate_101 = SARProcessing.get_coordinate(test_dem,(101,101))
+    index_101 = round.(Int,SARProcessing.get_index(test_dem,coordinate_101))
     
     ## Assert
-    testOk = coordinate_1 == coordinateOffset 
-    testOk &= coordinate_101 == (20.5,40.5)
-    testOk &= index_101 == (101,101)
+    test_ok = coordinate_1 == coordinate_offset 
+    test_ok &= coordinate_101 == (20.5,40.5)
+    test_ok &= index_101 == (101,101)
 
     ## Debug
-    if !testOk
+    if !test_ok
         println("Debug info: ", string(StackTraces.stacktrace()[1].func))
         println("coordinate_1: ", coordinate_1)
         println("coordinate_101: ", coordinate_101)
         println("index_101: ", index_101)
     end
 
-    return testOk
+    return test_ok
 
 end
 
 
 # Requires a TandemxDEM which can not be put on github due to licence.
-# The test is commented out.
-function loadTandemxDEM_test() 
+function load_tandemx_dem_test() 
     ## Arrange
-    testPath = "testData/largeFiles/TDM1_DEM__30_N38W028_V01_C/DEM/TDM1_DEM__30_N38W028_DEM.tif"
 
     ## Act
-    testdem = SARProcessing.loadTandemxDEM(testPath)
+    test_dem = SARProcessing.load_tandemx_dem(TEST_DEM)
     
     ## Assert
-    index_island = round.(Int,SARProcessing.getIndex(testdem,(38.7011104,-27.2354222))) ## coordinates on island
-    index_ocean =  round.(Int,SARProcessing.getIndex(testdem,(38.3592399,-27.3606715))) ## coordinates middle of ocean
+    index_island = round.(Int,SARProcessing.get_index(test_dem,(38.7011104,-27.2354222))) ## coordinates on island
+    index_ocean =  round.(Int,SARProcessing.get_index(test_dem,(38.3592399,-27.3606715))) ## coordinates middle of ocean
 
-    testOk = !ismissing(testdem.heights[index_island...]) && ismissing(testdem.heights[index_ocean...])
+    test_ok = !isnan(test_dem.heights[index_island...]) && isnan(test_dem.heights[index_ocean...])
     
     ## Debug
-    if !testOk
+    if !test_ok
         println("Debug info: ", string(StackTraces.stacktrace()[1].func))
-        println("testdem.heights[index_island]: ", testdem.heights[index_island...])
-        println("testdem.heights[index_ocean]: ", testdem.heights[index_ocean...])
+        println("test_dem.heights[index_island]: ", test_dem.heights[index_island...])
+        println("test_dem.heights[index_ocean]: ", test_dem.heights[index_ocean...])
     end
 
-    return testOk
+    return test_ok
 end
 
 
 @testset "DEM.jl" begin
     @test tandemxDEM_test()
     @test coordinate_index_test()
-    #@test loadTandemxDEM_test() # Requires a TandemxDEM which can not be put on github
+    if ispath(TEST_DEM)
+        @test load_tandemx_dem_test() # Requires a TandemxDEM which can not be put on github
+    end
 end
