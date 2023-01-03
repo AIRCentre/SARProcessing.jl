959ed22b4f3430356b8bde1ed18e971b9dc3199c
eyhl <eyhli@dtu.dk>
2023-01-02 22:11:57 -0500

    fixed requested changes

--- test/InSAR/InSARTest.jl
+++ test/InSAR/InSARTest.jl
@@ -2,8 +2,11 @@
 Checks the validity of the helper functions (polynomials) _doppler_fm_rate and _doppler_centroid_frequency
 """
 function polynomial_test(f, x, param, x0)
+    # Arrang
     # check if output is real
     output = f(x, param, x0);
+
+    # Act
     output_type = eltype(output);
     check_real = output_type <: Real
 
@@ -15,27 +18,74 @@ function polynomial_test(f, x, param, x0)
     check_monotonically_decreasing = [output[i+1] <= output[i] for i in eachindex(output[1:end-1])];
     check_monotonic = all(check_monotonically_increasing) | all(check_monotonically_decreasing);
 
+    # Assert
     test_ok = all([check_real, check_non_zero, check_monotonic]);
 
     if !test_ok
-        println([check_real, check_non_zero, check_monotonic])
         println("Debug info: ", string(StackTraces.stacktrace()[1].func))
         println("eltype(_doppler_fm_rate(x, param, x0)): ", output_type)
-        if !all(check_monotonically_increasing)
-            println("Index for breaking monotonic increase: ", findall(check_monotonically_increasing))
-        elseif !all(check_monotonically_decreasing)
-            println("Index for breaking monotonic decrease: ", findall(check_monotonically_decreasing))
-        else
-            println("Function is monotonic")
-        end
+        println("Is monotonically increasing: ", all(check_monotonically_increasing))
+        println("Is monotonically decreasing: ", all(check_monotonically_decreasing))
     end
 
     return test_ok
 end
 
-function test_phase_ramp(rows, columns, burst_number, mid_burst_speed, meta_data, orbit_states)
+function _doppler_fm_rate_test()
+    # Load
+    image = load_test_slc_image();
+    burst_number = SARProcessing.get_burst_numbers(image)[1]
+    metadata = image.metadata;
+    tau_0 = metadata.image.slant_range_time_seconds; 
+    Delta_tau_s = 1/metadata.product.range_sampling_rate;
+    columns = 1:metadata.image.number_of_samples;
+    range_time = tau_0 .+ (collect(columns) .- 1) .* Delta_tau_s
+    fm_meta = metadata.bursts[burst_number].azimuth_fm_rate;
+
+    # Test
+    polynomial_test(SARProcessing._doppler_fm_rate, range_time, fm_meta.polynomial, fm_meta.t0)
+end
+
+function _doppler_centroid_frequency_test()
+    # Load
+    image = load_test_slc_image();
+    burst_number = SARProcessing.get_burst_numbers(image)[1]
+    metadata = image.metadata;
+    tau_0 = metadata.image.slant_range_time_seconds; 
+    Delta_tau_s = 1/metadata.product.range_sampling_rate;
+    columns = 1:metadata.image.number_of_samples;
+    range_time = tau_0 .+ (collect(columns) .- 1) .* Delta_tau_s
+    dc_meta = metadata.bursts[burst_number].doppler_centroid;
+
+    # Test
+    polynomial_test(SARProcessing._doppler_centroid_frequency, range_time, dc_meta.polynomial, dc_meta.t0)
+end
+
+function phase_ramp_test()
+    ## Arrange
+    # load image
+    image = load_test_slc_image();
+    meta_data = image.metadata;
+    burst_number = SARProcessing.get_burst_numbers(image)[1]
+
+    # get range times from columns/samples
+    orbit_states = SARProcessing.load_precise_orbit_sentinel1(PRECISE_ORBIT_TEST_FILE2)
+
+    # ## Get mid burst time and speed
+    interpolator = SARProcessing.orbit_state_interpolator(orbit_states, image)
+    mid_burst_state = SARProcessing.get_burst_mid_states(image, interpolator)
+    mid_burst_speed = SARProcessing.get_speed.(mid_burst_state)[1]
+
+    # Create some matrices of row and colomn indeces
+    image_window = SARProcessing.get_window(image)
+    columns = collect(image_window[2][1]:image_window[2][2])
+    rows = collect(image_window[1][1]:image_window[1][2])
+
+    ## Act
+    ramp = SARProcessing.phase_ramp(rows, columns, burst_number, mid_burst_speed, meta_data);
+
+    ## Assert
     # check if output is real
-    ramp = SARProcessing.phase_ramp(rows, columns, burst_number, mid_burst_speed, meta_data, orbit_states);
     output_type = eltype(ramp);
     check_real = output_type <: Real
 
@@ -49,10 +99,31 @@ function test_phase_ramp(rows, columns, burst_number, mid_burst_speed, meta_data
     return test_ok
 end
 
-function test_deramp(image, rows, columns, burst_number, mid_burst_speed, meta_data, orbit_states)
-    ramp = SARProcessing.phase_ramp(rows, columns, burst_number, mid_burst_speed, meta_data, orbit_states);
+function deramp_test()
+    ## Arrange
+    # load image
+    image = load_test_slc_image();
+    meta_data = image.metadata;
+    burst_number = SARProcessing.get_burst_numbers(image)[1]
+
+    # get range times from columns/samples
+    orbit_states = SARProcessing.load_precise_orbit_sentinel1(PRECISE_ORBIT_TEST_FILE2)
+
+    # ## Get mid burst time and speed
+    interpolator = SARProcessing.orbit_state_interpolator(orbit_states, image)
+    mid_burst_state = SARProcessing.get_burst_mid_states(image,interpolator)
+    mid_burst_speed = SARProcessing.get_speed.(mid_burst_state)[1]
+
+    # Create some matrices of row and colomn indeces
+    image_window = SARProcessing.get_window(image)
+    columns = collect(image_window[2][1]:image_window[2][2])
+    rows = collect(image_window[1][1]:image_window[1][2])
+
+    ## Act 
+    ramp = SARProcessing.phase_ramp(rows, columns, burst_number[1], mid_burst_speed, meta_data);
     deramped_image = SARProcessing.deramp(image, ramp)
 
+    ## Assert
     output_type = eltype(deramped_image);
     check_real = output_type <: Complex
 
@@ -63,9 +134,32 @@ function test_deramp(image, rows, columns, burst_number, mid_burst_speed, meta_d
     return test_ok
 end
 
-function test_reramp(image, rows, columns, burst_number, mid_burst_speed, meta_data, orbit_states)
-    ramp = SARProcessing.phase_ramp(rows, columns, burst_number, mid_burst_speed, meta_data, orbit_states);
+function reramp_test()
+    ## Arrange
+    # load image
+    image = load_test_slc_image();
+    meta_data = image.metadata;
+    burst_number = SARProcessing.get_burst_numbers(image)[1]
+
+    # get range times from columns/samples
+    orbit_states = SARProcessing.load_precise_orbit_sentinel1(PRECISE_ORBIT_TEST_FILE2)
+
+    # ## Get mid burst time and speed
+    interpolator = SARProcessing.orbit_state_interpolator(orbit_states, image)
+    mid_burst_state = SARProcessing.get_burst_mid_states(image,interpolator)
+    mid_burst_speed = SARProcessing.get_speed.(mid_burst_state)[1]
+
+    # Create some matrices of row and colomn indeces
+    image_window = SARProcessing.get_window(image)
+    columns = collect(image_window[2][1]:image_window[2][2])
+    rows = collect(image_window[1][1]:image_window[1][2])
+
+    ramp = SARProcessing.phase_ramp(rows, columns, burst_number[1], mid_burst_speed, meta_data);
+
+    ## Act
     reramped_image = SARProcessing.reramp(image, ramp)
+
+    ## Assert
     output_type = eltype(reramped_image);
     check_real = output_type <: Complex
 
@@ -77,44 +171,10 @@ function test_reramp(image, rows, columns, burst_number, mid_burst_speed, meta_d
 end
 
 @testset "InSARTest.jl" begin
-    image = load_test_slc_image();
-    metadata = image.metadata;
-    index_start = image.index_start;
-    columns = 1:metadata.image.number_of_samples;
-    tau_0 = metadata.image.slant_range_time.value; 
-    Delta_tau_s = 1/metadata.product.range_sampling_rate;
-
-    # get range times from columns/samples
-    range_time = tau_0 .+ (collect(columns) .- 1) .* Delta_tau_s
-    orbit_states = SARProcessing.load_precise_orbit_sentinel1(PRECISE_ORBIT_TEST_FILE2)
-
-    # ## Get mid burst time and speed
-    interpolator = SARProcessing.orbit_state_interpolator(orbit_states, image)
-    mid_burst_state = SARProcessing.get_burst_mid_states(image,interpolator)
-    mid_burst_speed = SARProcessing.get_speed.(mid_burst_state)
-
-    # test all bursts
-    for (i, burst_number) in enumerate(SARProcessing.get_burst_numbers(image))
-        fm_meta = metadata.bursts[burst_number].azimuth_fm_rate;
-        @test polynomial_test(SARProcessing._doppler_fm_rate, range_time, fm_meta.polynomial, fm_meta.t0)
-
-        dc_meta = metadata.bursts[burst_number].doppler_centroid;
-        @test polynomial_test(SARProcessing._doppler_centroid_frequency, range_time, dc_meta.polynomial, dc_meta.t0)
-
-        # Create some matrices of row and colomn indeces
-        image_window = SARProcessing.get_window(image)
-        columns = collect(image_window[2][1]:image_window[2][2])
-        rows = collect(image_window[1][1]:image_window[1][2])
-
-        n_columns = length(columns)
-        columns = repeat(columns', length(rows))
-        rows = Matrix(transpose(repeat(rows', n_columns)))
-
-        @test test_phase_ramp(rows, columns, burst_number, mid_burst_speed[i], metadata, orbit_states)
-
-        @test test_deramp(image, rows, columns, burst_number, mid_burst_speed[i], metadata, orbit_states)
-        @test test_reramp(image, rows, columns, burst_number, mid_burst_speed[i], metadata, orbit_states)
-
-        #TODO: Add more phase ramp tests
-    end
+    @test _doppler_centroid_frequency_test()
+    @test _doppler_fm_rate_test()
+    @test phase_ramp_test()
+    @test deramp_test()
+    @test reramp_test()
+    #TODO: Add more phase ramp tests
 end
