35b5d0446631af41900f927f2b2afdd5cfd48fef
Kristian Aalling Sørensen <kristian.sorensen@hotmail.com>
2023-01-06 23:02:43 +0100

    updated notebooks and functions for object detect (#49)

--- test/object_detector/object_detector_cfar_test.jl
+++ test/object_detector/object_detector_cfar_test.jl
@@ -4,7 +4,7 @@
 
 
 
-function ca_cfar_test()
+function tp_cfar_test()
 
     image_no_objects = [i for i=1:0.5:20.5, j=1:40];
     image_with_objects =[i for i=1:0.5:20.5, j=1:40]
@@ -13,8 +13,8 @@ function ca_cfar_test()
     image_with_objects[20:30,20:33] .= 60000; #large object
 
 
-    cfar1 = SARProcessing.cell_averaging_constant_false_alarm_rate(image_no_objects,5,3,0.01);
-    cfar2 = SARProcessing.cell_averaging_constant_false_alarm_rate(image_with_objects,5,3,0.01);
+    cfar1 = SARProcessing.two_parameter_constant_false_alarm_rate(image_no_objects,5,3,0.01);
+    cfar2 = SARProcessing.two_parameter_constant_false_alarm_rate(image_with_objects,5,3,0.01);
 
     check = sum(cfar2)==8;
     check &= sum(cfar1)==0;
@@ -55,6 +55,6 @@ end
 
 @testset "object_detector_cfar_test.jl" begin
     ####### actual tests ###############
-    @test ca_cfar_test()
+    @test tp_cfar_test()
     @test cp_cfar_test()
 end
